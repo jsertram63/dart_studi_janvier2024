896f153e236d49350188319a87af71eaa93da542
jsertram63 <54062083+jsertram63@users.noreply.github.com>
2024-01-15 18:53:23 +0100

    live 2

--- bin/dart_live_studi_janvier2024.dart
+++ bin/dart_live_studi_janvier2024.dart
@@ -42,7 +42,7 @@ void main(List<String> arguments) {
   // 
 
 
-  // des opérateurs arithmétiques + , - , / * , %
+  // des opérateurs arithmétiques + , - , / * , %  12%2 13%2 1
   int unEntier = 10;
   unEntier +=2;
   print(unEntier);
@@ -70,6 +70,106 @@ void main(List<String> arguments) {
     print("Procure toi cle et digicode");
    }
 
+
+   // Structure de controle
+   // while / do while
+
+   print("-----------------------------------------");
+  var i = 0;
+  while(i<5){
+    print("$i bonjour tout le monde");
+    i++;
+  }
+
+  var t= 5;
+  do {
+    print("$t BOnjour avec un do While");
+    t++;
+    var x = 0;
+    while (x<2){
+      x++;
+      print("$x je suis dans la boucle imbriqué");
+    }
+   } while(t<4);
+
+
+
+   var p = 0;
+   print("------------------------");
+   print("Mot-clé break");
+  while(p<20){
+    if (p== 2){
+      print("je vais sortir");
+      break;
+    }
+    p++;
+  print("$p");
+
+  }
+
+  // 2eme façon de faire boucle : mot-clé for
+  print("=============================");
+  print("BOUCLE FOR");
+
+  for(var i= 0; i< 5; i++){
+    print("$i bonjour tout le monde avec une boucle for");
+  }
+
+  String maChaine = "une premiere chaine de caractère";
+  String maChaine2 = "une deuxieme chaine";
+
+  String multiLigne = """ bonjour 
+    les apprenants
+    sur plusieurs lignes """;
+  print(multiLigne);
+
+  print(maChaine.toUpperCase());
+  print(maChaine.codeUnits);
+  print("La longueur de la chaine est de ${maChaine2.length}");
+
+  print(maChaine.substring(0,8).toUpperCase());
+
+  // la première structure de controle if / else => si / sinon
+  var sportPratique = "foot";
+
+  if (sportPratique == "foot"){
+    print("Je pratique le foot");
+  }else {
+    print("je pratique $sportPratique");
+  }
+
+  print("=====================================");
+  print("SWITCH");
+  print("=====================================");
+  // deuxieme façon
+  switch(sportPratique){
+    case'foot': print("je pratique le foot");
+    break;
+    default: print("Je pratique $sportPratique");
+  }
+
+
+  var emoji = "\u{13faf}";
+  print(emoji);
+
+  // Les fonctions 
+  // type de fonction + nomDeLaFonction + des arguments (facultatifs)
+  String AfficheMessageBienvenue(){
+    var messageDeBienvenue ="";
+     messageDeBienvenue += "BOnjour";
+    messageDeBienvenue += " tout le monde";
+    return messageDeBienvenue;
+
+  }
+
+  var message = AfficheMessageBienvenue();
+  print(message);
+
+  AfficheMessageBienvenue();
+   AfficheMessageBienvenue();
+
+
+  
 
 
 
